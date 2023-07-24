c2846a2555bd2f4f5995f3d5c4d964984a07547e
RAGiordano <profesorgiordano@gmail.com>
2023-07-24 19:40:38 -0300

    Guia_JUnit-Ejercicio2 hecho con un test

---
+++ Guia_JUnit-Ejercicio2/src/test/java/NewEmptyJUnitTest.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import Services.TemperatureConverterService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NewEmptyJUnitTest {
+    
+    public NewEmptyJUnitTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testTempConvertions() throws Exception {
+    System.out.println("Testeo de distintos tipos de conversiones");
+    TemperatureConverterService TCS = new TemperatureConverterService();
+    double expResult = 77;
+    double result = TCS.convert("celsius","fahrenheit", 25);
+    assertEquals(expResult, result, 0.0);
+    }
+}
